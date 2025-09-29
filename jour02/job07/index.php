8f55543a835f6ec6aaf22731aa235c9952eeb88b
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-09-29 14:46:49 +0200

    MAJ: triangle jour02/job07

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -3,34 +3,83 @@ Faire un algorithme qui aﬃche un triangle de 5 de hauteur. Cette dimension dev
 stockée dans une variable $hauteur, modifiable facilement.
 -->
 <?php
+$hauteur = 5;       // Hauteur du triangle
+$gauche = '/';
+$droite = '\\';
+$baseChar = '_';
+$espace = '&nbsp;'; // espace HTML non-breakable
 
-$height = 8;
-$leftChar = '/';
-$rightChar = '\\';
-$space = '&nbsp;&nbsp;';
-$spaceAlone = '&nbsp;';
+echo '<pre>';
 
-$innerSpace = '';
-$foot = '';
-$countA = 0;
-$countB = 0;
+for ($ligne = 0; $ligne < $hauteur - 1; $ligne++) {
 
-// Boucle pour chaque niveau du triangle
-for ($countA = 1; $countA < $height; $countA++) {
-    $outerSpace = '';
-    for ($countOuterSpaces = 0; $countOuterSpaces < ($height - 1 - $countA); $countOuterSpaces++) {
-        $outerSpace .= $spaceAlone;
+    // Espaces extérieurs
+    $espacesExt = '';
+    for ($i = 0; $i < $hauteur - $ligne - 1; $i++) {
+        $espacesExt .= $espace;
     }
-    // Ajout des traits obliques
-    echo $outerSpace . $spaceAlone . $leftChar . $innerSpace . $rightChar . "<br />";
-    // Espace intérieur pour le prochain niveau
-    $innerSpace .= $space . $space;
+
+    // Espaces intérieurs
+    $espacesInt = '';
+    if ($ligne != 0) {
+        for ($j = 0; $j < 2 * $ligne; $j++) {
+            $espacesInt .= $espace;
+        }
+    }
+
+    // Affichage de la ligne
+    echo $espacesExt . $gauche . $espacesInt . $droite . "<br />";
 }
 
 // Base du triangle
-for ($countB = 0; $countB < (2 * $height - 1); $countB++) {
-    $foot .= '_';
+echo $gauche;
+for ($i = 0; $i < 2 * ($hauteur - 1); $i++) {
+    echo $baseChar;
 }
-echo $foot;
+echo $droite;
 
+echo '</pre>';
 ?>
+
+<!-- =========================================
+     NOTICE PÉDAGOGIQUE
+=========================================
+
+TRIANGLE ASCII - Job07
+
+Objectif :
+- Afficher un triangle droit de hauteur $hauteur
+- Centrer le triangle dans le navigateur
+- Rendre le tout lisible avec &nbsp;
+
+Explications pas à pas :
+1. Boucle principale ($ligne)
+   - Parcourt chaque niveau du triangle (sauf la base)
+   - Permet de construire le toit ligne par ligne
+
+2. Boucles imbriquées
+   a) Espaces extérieurs
+      - Centrent le triangle
+      - Nombre = $hauteur - $ligne - 1
+   b) Espaces intérieurs
+      - Augmentent à chaque ligne pour élargir le triangle
+      - Nombre = 2 * $ligne
+
+3. Affichage
+   - Chaque ligne = espaces extérieurs + / + espaces intérieurs + \
+   - <br /> pour passer à la ligne suivante
+   - La base est affichée séparément
+
+Ce que tu apprends :
+- Utilisation de boucles imbriquées
+- Gestion du centrage dans un affichage ASCII
+- Construction progressive d’une figure géométrique
+- Rendu correct dans le navigateur avec &nbsp;
+
+Rubrique technique :
+- Boucle principale + boucles internes (imbriquées)
+- Condition pour gérer le sommet (pas d’espace intérieur)
+- Boucle séparée pour la base
+- HTML <pre> ou <br /> pour conserver l’alignement
+
+-->
