2cc344ea6ec2fdc71807baa6e928c09b172dffa4
stephane-nawrat <stephane.nawrat@laplateforme.io>
2025-09-30 09:53:15 +0200

    Ajout: première fonction PHP hello()

---
+++ jour07/job01/index.php
@@ -0,0 +1,15 @@
+<!--  jour07/job01
+Créez une fonction nommée “hello()”. Cette fonction, une fois appelée, doit aﬃcher sur
+la page : “Hello LaPlateforme!”.
+-->
+<?php
+
+// Déclaration de la fonction
+function hello() {
+    echo "Hello LaPlateforme!";
+}
+
+// Appel de la fonction
+hello();
+
+?>
